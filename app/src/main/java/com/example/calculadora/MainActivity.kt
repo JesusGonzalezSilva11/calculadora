5c9745800eadb6ee60d769453c26b2d45a12fcb5
jesus <jesusgonzalezsilva11@gmail.com>
2023-12-15 09:26:37 +0100

    com2

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -51,7 +51,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.ui.res.painterResource
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {//master
         super.onCreate(savedInstanceState)
         setContent {
             CalculadoraTheme {
